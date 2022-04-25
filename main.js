03e2ae386fc732e7bba002f9f11f293493016e7c
Poorvi1010 <91541629+Poorvi1010@users.noreply.github.com>
2022-04-25 08:04:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,4 +12,36 @@ function Capture_Image()
  Webcam.snap(function(data_uri){
      document.getElementById("result").innerHTML="<img id='captured_image'src='"+data_uri+"'>"
  });
+ console.log("ml5.version",ml5.version);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/3g50WrLgY/model.json",modelLoaded);
+function modelLoaded(){
+    console.log("Model Loaded");
+}
+}
+function Snapshot()
+{
+    img=document.getElementById("captured_image");
+    classifier.classify(img,gotResults);
+}
+function gotResults(error,result)
+{
+if (error) {
+    console.log(error)
+} else {
+    console.log(result)
+    document.getElementById("result_gesture").innerHTML=result[0].label;
+}
+if(result[0].label=="Peaceout")
+{
+    document.getElementById("update_gesture").innerHTML="&#x270C;";
+}
+if(result[0].label=="goodjob")
+{
+    document.getElementById("update_gesture").innerHTML="&#x1F44C;";
+}
+if(result[0].label=="thumbsup")
+{
+    document.getElementById("update_gesture").innerHTML="&#x1F44D;";
+}
+
 }
