822a47fe1c0ee07e8ee37dafd6fba8de9b446d9b
Poorvi1010 <91541629+Poorvi1010@users.noreply.github.com>
2022-06-16 18:03:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function Capture_Image()
      document.getElementById("result").innerHTML="<img id='captured_image'src='"+data_uri+"'>"
  });
  console.log("ml5.version",ml5.version);
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/3g50WrLgY/model.json",modelLoaded);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/E1LpGX7Vu/model.json",modelLoaded);
 function modelLoaded(){
     console.log("Model Loaded");
 }
